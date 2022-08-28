75a616e2362109e904b2c611384a43a58c04d6d4
syed-ahmed0 <102703936+syed-ahmed0@users.noreply.github.com>
2022-08-28 00:15:51 -0400

    Add files via upload

---
+++ episodeList.js
@@ -0,0 +1,770 @@
+export const Episode = [
+    {
+        "FIELD1": "Season 1, Episode 1",
+        "FIELD2": "The Pilot"
+    },
+    {
+        "FIELD1": "Season 1, Episode 2",
+        "FIELD2": "Diversity Day"
+    },
+    {
+        "FIELD1": "Season 1, Episode 3",
+        "FIELD2": "Health Care"
+    },
+    {
+        "FIELD1": "Season 1, Episode 4",
+        "FIELD2": "The Alliance"
+    },
+    {
+        "FIELD1": "Season 1, Episode 5",
+        "FIELD2": "Basketball"
+    },
+    {
+        "FIELD1": "Season 1, Episode 6",
+        "FIELD2": "Hot Girl"
+    },
+    {
+        "FIELD1": "Season 2, Episode 1",
+        "FIELD2": "The Dundies"
+    },
+    {
+        "FIELD1": "Season 2, Episode 2",
+        "FIELD2": "Sexual Harrasment"
+    },
+    {
+        "FIELD1": "Season 2, Episode 3",
+        "FIELD2": "Office Olympics"
+    },
+    {
+        "FIELD1": "Season 2, Episode 4",
+        "FIELD2": "The Fire"
+    },
+    {
+        "FIELD1": "Season 2, Episode 5",
+        "FIELD2": "Halloween"
+    },
+    {
+        "FIELD1": "Season 2, Episode 6",
+        "FIELD2": "The Fight"
+    },
+    {
+        "FIELD1": "Season 2, Episode 7",
+        "FIELD2": "The Client"
+    },
+    {
+        "FIELD1": "Season 2, Episode 8",
+        "FIELD2": "Performance Review"
+    },
+    {
+        "FIELD1": "Season 2, Episode 9",
+        "FIELD2": "Email Surveillance"
+    },
+    {
+        "FIELD1": "Season 2, Episode 10",
+        "FIELD2": "Christmas Party"
+    },
+    {
+        "FIELD1": "Season 2, Episode 11",
+        "FIELD2": "Booze Cruise"
+    },
+    {
+        "FIELD1": "Season 2, Episode 12",
+        "FIELD2": "The Injury"
+    },
+    {
+        "FIELD1": "Season 2, Episode 13",
+        "FIELD2": "The Secret"
+    },
+    {
+        "FIELD1": "Season 2, Episode 14",
+        "FIELD2": "The Carpet"
+    },
+    {
+        "FIELD1": "Season 2, Episode 15",
+        "FIELD2": "Boys and Girls"
+    },
+    {
+        "FIELD1": "Season 2, Episode 16",
+        "FIELD2": "Valentine's day"
+    },
+    {
+        "FIELD1": "Season 2, Episode 17",
+        "FIELD2": "Dwight's Speech"
+    },
+    {
+        "FIELD1": "Season 2, Episode 18",
+        "FIELD2": "Take Your Daughter to Work Day"
+    },
+    {
+        "FIELD1": "Season 2, Episode 19",
+        "FIELD2": "Michael's Birthday"
+    },
+    {
+        "FIELD1": "Season 2, Episode 20",
+        "FIELD2": "Drug testing"
+    },
+    {
+        "FIELD1": "Season 2, Episode 21",
+        "FIELD2": "Conflict Resolution"
+    },
+    {
+        "FIELD1": "Season 2, Episode 22",
+        "FIELD2": "Casino Night"
+    },
+    {
+        "FIELD1": "Season 3, Episode 1",
+        "FIELD2": "Gay Witch Hunt"
+    },
+    {
+        "FIELD1": "Season 3, Episode 2",
+        "FIELD2": "The Convention"
+    },
+    {
+        "FIELD1": "Season 3, Episode 3",
+        "FIELD2": "The Coup"
+    },
+    {
+        "FIELD1": "Season 3, Episode 4",
+        "FIELD2": "Grief Counselling"
+    },
+    {
+        "FIELD1": "Season 3, Episode 5",
+        "FIELD2": "Initiation"
+    },
+    {
+        "FIELD1": "Season 3, Episode 6",
+        "FIELD2": "Diwali"
+    },
+    {
+        "FIELD1": "Season 3, Episode 7",
+        "FIELD2": "Branch Closing"
+    },
+    {
+        "FIELD1": "Season 3, Episode 8",
+        "FIELD2": "The Merger"
+    },
+    {
+        "FIELD1": "Season 3, Episode 9",
+        "FIELD2": "The Convict"
+    },
+    {
+        "FIELD1": "Season 3, Episode 10",
+        "FIELD2": "A Benihana Chirstmas"
+    },
+    {
+        "FIELD1": "Season 3, Episode 11",
+        "FIELD2": "Back From Vacation"
+    },
+    {
+        "FIELD1": "Season 3, Episode 12",
+        "FIELD2": "Travelling Salesman"
+    },
+    {
+        "FIELD1": "Season 3, Episode 13",
+        "FIELD2": "The Return"
+    },
+    {
+        "FIELD1": "Season 3, Episode 14",
+        "FIELD2": "Ben Franklin"
+    },
+    {
+        "FIELD1": "Season 3, Episode 15",
+        "FIELD2": "Phyllis' Wedding"
+    },
+    {
+        "FIELD1": "Season 3, Episode 16",
+        "FIELD2": "Buisness School"
+    },
+    {
+        "FIELD1": "Season 3, Episode 17",
+        "FIELD2": "Cocktails"
+    },
+    {
+        "FIELD1": "Season 3, Episode 18",
+        "FIELD2": "The Negotiaion"
+    },
+    {
+        "FIELD1": "Season 3, Episode 19",
+        "FIELD2": "Safety Training"
+    },
+    {
+        "FIELD1": "Season 3, Episode 20",
+        "FIELD2": "Product Recall"
+    },
+    {
+        "FIELD1": "Season 3, Episode 21",
+        "FIELD2": "Women's Appreciation"
+    },
+    {
+        "FIELD1": "Season 3, Episode 22",
+        "FIELD2": "Beach Games"
+    },
+    {
+        "FIELD1": "Season 3, Episode 23",
+        "FIELD2": "The Job"
+    },
+    {
+        "FIELD1": "Season 4, Episode 1",
+        "FIELD2": "Fun Run"
+    },
+    {
+        "FIELD1": "Season 4, Episode 2",
+        "FIELD2": "Dunder Mifflin Infinity"
+    },
+    {
+        "FIELD1": "Season 4, Episode 3",
+        "FIELD2": "Launch Party"
+    },
+    {
+        "FIELD1": "Season 4, Episode 4",
+        "FIELD2": "Money"
+    },
+    {
+        "FIELD1": "Season 4, Episode 5",
+        "FIELD2": "Local Ad"
+    },
+    {
+        "FIELD1": "Season 4, Episode 6",
+        "FIELD2": "Branch Wars"
+    },
+    {
+        "FIELD1": "Season 4, Episode 7",
+        "FIELD2": "Survivor Man"
+    },
+    {
+        "FIELD1": "Season 4, Episode 8",
+        "FIELD2": "The Deposition"
+    },
+    {
+        "FIELD1": "Season 4, Episode 9",
+        "FIELD2": "Dinner Party"
+    },
+    {
+        "FIELD1": "Season 4, Episode 10",
+        "FIELD2": "The Chair Model"
+    },
+    {
+        "FIELD1": "Season 4, Episode 11",
+        "FIELD2": "Night Out"
+    },
+    {
+        "FIELD1": "Season 4, Episode 12",
+        "FIELD2": "Did I Stutter"
+    },
+    {
+        "FIELD1": "Season 4, Episode 13",
+        "FIELD2": "Job Fair"
+    },
+    {
+        "FIELD1": "Season 4, Episode 14",
+        "FIELD2": "Goodbye, Toby"
+    },
+    {
+        "FIELD1": "Season 5, Episode 1",
+        "FIELD2": "Weight Loss: Part 1"
+    },
+    {
+        "FIELD1": "Season 5, Episode 2",
+        "FIELD2": "Weight Loss: Part 2"
+    },
+    {
+        "FIELD1": "Season 5, Episode 3",
+        "FIELD2": "Buisness Ethics"
+    },
+    {
+        "FIELD1": "Season 5, Episode 4",
+        "FIELD2": "Baby Shower"
+    },
+    {
+        "FIELD1": "Season 5, Episode 5",
+        "FIELD2": "Crime Aid"
+    },
+    {
+        "FIELD1": "Season 5, Episode 6",
+        "FIELD2": "Employee Transfer"
+    },
+    {
+        "FIELD1": "Season 5, Episode 7",
+        "FIELD2": "Customer Survey"
+    },
+    {
+        "FIELD1": "Season 5, Episode 8",
+        "FIELD2": "Buisness Trip"
+    },
+    {
+        "FIELD1": "Season 5, Episode 9",
+        "FIELD2": "Frame Toby"
+    },
+    {
+        "FIELD1": "Season 5, Episode 10",
+        "FIELD2": "The Surplus"
+    },
+    {
+        "FIELD1": "Season 5, Episode 11",
+        "FIELD2": "Morrocan Christmas"
+    },
+    {
+        "FIELD1": "Season 5, Episode 12",
+        "FIELD2": "The Duel"
+    },
+    {
+        "FIELD1": "Season 5, Episode 13",
+        "FIELD2": "Prince Family Paper"
+    },
+    {
+        "FIELD1": "Season 5, Episode 14",
+        "FIELD2": "Stress Relief: Part 1"
+    },
+    {
+        "FIELD1": "Season 5, Episode 15",
+        "FIELD2": "Stress Relief: Part 2"
+    },
+    {
+        "FIELD1": "Season 5, Episode 16",
+        "FIELD2": "Lecture Circuit: Part 1"
+    },
+    {
+        "FIELD1": "Season 5, Episode 17",
+        "FIELD2": "Lecture Circuit: Part 2"
+    },
+    {
+        "FIELD1": "Season 5, Episode 18",
+        "FIELD2": "Blood Drive"
+    },
+    {
+        "FIELD1": "Season 5, Episode 19",
+        "FIELD2": "Golden Ticket"
+    },
+    {
+        "FIELD1": "Season 5, Episode 20",
+        "FIELD2": "New Boss"
+    },
+    {
+        "FIELD1": "Season 5, Episode 21",
+        "FIELD2": "Two Weeks"
+    },
+    {
+        "FIELD1": "Season 5, Episode 22",
+        "FIELD2": "Dream Team"
+    },
+    {
+        "FIELD1": "Season 5, Episode 23",
+        "FIELD2": "Michael Scott Paper Company"
+    },
+    {
+        "FIELD1": "Season 5, Episode 24",
+        "FIELD2": "Heavy Competition"
+    },
+    {
+        "FIELD1": "Season 5, Episode 25",
+        "FIELD2": "Broke"
+    },
+    {
+        "FIELD1": "Season 5, Episode 26",
+        "FIELD2": "Casual Friday"
+    },
+    {
+        "FIELD1": "Season 5, Episode 27",
+        "FIELD2": "Cafe Disco"
+    },
+    {
+        "FIELD1": "Season 5, Episode 28",
+        "FIELD2": "Company Picnic"
+    },
+    {
+        "FIELD1": "Season 6, Episode 1",
+        "FIELD2": "Gossip"
+    },
+    {
+        "FIELD1": "Season 6, Episode 2",
+        "FIELD2": "The Meeting"
+    },
+    {
+        "FIELD1": "Season 6, Episode 3",
+        "FIELD2": "The Promotion"
+    },
+    {
+        "FIELD1": "Season 6, Episode 4",
+        "FIELD2": "Niagara: Part 1"
+    },
+    {
+        "FIELD1": "Season 6, Episode 5",
+        "FIELD2": "Niagara: Part 2"
+    },
+    {
+        "FIELD1": "Season 6, Episode 6",
+        "FIELD2": "Mafia"
+    },
+    {
+        "FIELD1": "Season 6, Episode 7",
+        "FIELD2": "The Lover"
+    },
+    {
+        "FIELD1": "Season 6, Episode 8",
+        "FIELD2": "Koi Pond"
+    },
+    {
+        "FIELD1": "Season 6, Episode 9",
+        "FIELD2": "Double Date"
+    },
+    {
+        "FIELD1": "Season 6, Episode 10",
+        "FIELD2": "Murder"
+    },
+    {
+        "FIELD1": "Season 6, Episode 11",
+        "FIELD2": "Shareholder Meeting"
+    },
+    {
+        "FIELD1": "Season 6, Episode 12",
+        "FIELD2": "Scott's Tots"
+    },
+    {
+        "FIELD1": "Season 6, Episode 13",
+        "FIELD2": "Secret Santa"
+    },
+    {
+        "FIELD1": "Season 6, Episode 14",
+        "FIELD2": "The Banker"
+    },
+    {
+        "FIELD1": "Season 6, Episode 15",
+        "FIELD2": "Sabre"
+    },
+    {
+        "FIELD1": "Season 6, Episode 16",
+        "FIELD2": "Manager and the Salesman"
+    },
+    {
+        "FIELD1": "Season 6, Episode 17",
+        "FIELD2": "Delivery: Part 1"
+    },
+    {
+        "FIELD1": "Season 6, Episode 18",
+        "FIELD2": "Delivery: Part 2"
+    },
+    {
+        "FIELD1": "Season 6, Episode 19",
+        "FIELD2": "St. Patrick's Day"
+    },
+    {
+        "FIELD1": "Season 6, Episode 20",
+        "FIELD2": "New Leads"
+    },
+    {
+        "FIELD1": "Season 6, Episode 21",
+        "FIELD2": "Happy Hour"
+    },
+    {
+        "FIELD1": "Season 6, Episode 22",
+        "FIELD2": "Secretary's Day"
+    },
+    {
+        "FIELD1": "Season 6, Episode 23",
+        "FIELD2": "Body Language"
+    },
+    {
+        "FIELD1": "Season 6, Episode 24",
+        "FIELD2": "The Cover-Up"
+    },
+    {
+        "FIELD1": "Season 6, Episode 25",
+        "FIELD2": "The Chump"
+    },
+    {
+        "FIELD1": "Season 6, Episode 26",
+        "FIELD2": "Whistleblower"
+    },
+    {
+        "FIELD1": "Season 7, Episode 1",
+        "FIELD2": "Nepotism"
+    },
+    {
+        "FIELD1": "Season 7, Episode 2",
+        "FIELD2": "Counseling"
+    },
+    {
+        "FIELD1": "Season 7, Episode 3",
+        "FIELD2": "Andy's Play"
+    },
+    {
+        "FIELD1": "Season 7, Episode 4",
+        "FIELD2": "Sex Ed"
+    },
+    {
+        "FIELD1": "Season 7, Episode 5",
+        "FIELD2": "The Sting"
+    },
+    {
+        "FIELD1": "Season 7, Episode 6",
+        "FIELD2": "Costume Contest"
+    },
+    {
+        "FIELD1": "Season 7, Episode 7",
+        "FIELD2": "Christening"
+    },
+    {
+        "FIELD1": "Season 7, Episode 8",
+        "FIELD2": "Viewing Party"
+    },
+    {
+        "FIELD1": "Season 7, Episode 9",
+        "FIELD2": "WUPHF.com"
+    },
+    {
+        "FIELD1": "Season 7, Episode 10",
+        "FIELD2": "China"
+    },
+    {
+        "FIELD1": "Season 7, Episode 11",
+        "FIELD2": "Classy Christmas: Part 1"
+    },
+    {
+        "FIELD1": "Season 7, Episode 12",
+        "FIELD2": "Classy Christmas: Part 2"
+    },
+    {
+        "FIELD1": "Season 7, Episode 13",
+        "FIELD2": "Ultimatum"
+    },
+    {
+        "FIELD1": "Season 7, Episode 14",
+        "FIELD2": "The Seminar"
+    },
+    {
+        "FIELD1": "Season 7, Episode 15",
+        "FIELD2": "The Search"
+    },
+    {
+        "FIELD1": "Season 7, Episode 16",
+        "FIELD2": "PDA"
+    },
+    {
+        "FIELD1": "Season 7, Episode 17",
+        "FIELD2": "Threat Level Midnight"
+    },
+    {
+        "FIELD1": "Season 7, Episode 18",
+        "FIELD2": "Todd Packer"
+    },
+    {
+        "FIELD1": "Season 7, Episode 19",
+        "FIELD2": "Garage Sale"
+    },
+    {
+        "FIELD1": "Season 7, Episode 20",
+        "FIELD2": "Training Day"
+    },
+    {
+        "FIELD1": "Season 7, Episode 21",
+        "FIELD2": "Michael's Last Dundies"
+    },
+    {
+        "FIELD1": "Season 7, Episode 22",
+        "FIELD2": "Goodbye, Michael"
+    },
+    {
+        "FIELD1": "Season 7, Episode 23",
+        "FIELD2": "The Inner Circle"
+    },
+    {
+        "FIELD1": "Season 7, Episode 24",
+        "FIELD2": "Dwight K. Schrute, (Acting) Manager"
+    },
+    {
+        "FIELD1": "Season 7, Episode 25",
+        "FIELD2": "Search Committee: Part 1"
+    },
+    {
+        "FIELD1": "Season 7, Episode 26",
+        "FIELD2": "Search Committee: Part 2"
+    },
+    {
+        "FIELD1": "Season 8, Episode 1",
+        "FIELD2": "The List"
+    },
+    {
+        "FIELD1": "Season 8, Episode 2",
+        "FIELD2": "The Incentive"
+    },
+    {
+        "FIELD1": "Season 8, Episode 3",
+        "FIELD2": "Lotto"
+    },
+    {
+        "FIELD1": "Season 8, Episode 4",
+        "FIELD2": "Garden Party"
+    },
+    {
+        "FIELD1": "Season 8, Episode 5",
+        "FIELD2": "Spooked"
+    },
+    {
+        "FIELD1": "Season 8, Episode 6",
+        "FIELD2": "Doomsday"
+    },
+    {
+        "FIELD1": "Season 8, Episode 7",
+        "FIELD2": "Pam's Replacement"
+    },
+    {
+        "FIELD1": "Season 8, Episode 8",
+        "FIELD2": "Gettysburg"
+    },
+    {
+        "FIELD1": "Season 8, Episode 9",
+        "FIELD2": "Mrs. California"
+    },
+    {
+        "FIELD1": "Season 8, Episode 10",
+        "FIELD2": "Christmas Wishes"
+    },
+    {
+        "FIELD1": "Season 8, Episode 11",
+        "FIELD2": "Trivia"
+    },
+    {
+        "FIELD1": "Season 8, Episode 12",
+        "FIELD2": "Pool Party"
+    },
+    {
+        "FIELD1": "Season 8, Episode 13",
+        "FIELD2": "Jury Duty"
+    },
+    {
+        "FIELD1": "Season 8, Episode 14",
+        "FIELD2": "Special Project"
+    },
+    {
+        "FIELD1": "Season 8, Episode 15",
+        "FIELD2": "Tallahassee"
+    },
+    {
+        "FIELD1": "Season 8, Episode 16",
+        "FIELD2": "After Hours"
+    },
+    {
+        "FIELD1": "Season 8, Episode 17",
+        "FIELD2": "Test The Store"
+    },
+    {
+        "FIELD1": "Season 8, Episode 18",
+        "FIELD2": "Last Day in Florida"
+    },
+    {
+        "FIELD1": "Season 8, Episode 19",
+        "FIELD2": "Get the Gir;"
+    },
+    {
+        "FIELD1": "Season 8, Episode 20",
+        "FIELD2": "Welcome Party"
+    },
+    {
+        "FIELD1": "Season 8, Episode 21",
+        "FIELD2": "Angry Andy"
+    },
+    {
+        "FIELD1": "Season 8, Episode 22",
+        "FIELD2": "Fundraiser"
+    },
+    {
+        "FIELD1": "Season 8, Episode 23",
+        "FIELD2": "Turf War"
+    },
+    {
+        "FIELD1": "Season 8, Episode 24",
+        "FIELD2": "Free Family Portrait Studio"
+    },
+    {
+        "FIELD1": "Season 9, Episode 1",
+        "FIELD2": "New Guys"
+    },
+    {
+        "FIELD1": "Season 9, Episode 2",
+        "FIELD2": "Roy's Wedding"
+    },
+    {
+        "FIELD1": "Season 9, Episode 3",
+        "FIELD2": "Andy's Ancestry"
+    },
+    {
+        "FIELD1": "Season 9, Episode 4",
+        "FIELD2": "Work Bus"
+    },
+    {
+        "FIELD1": "Season 9, Episode 5",
+        "FIELD2": "Here Comes Treble"
+    },
+    {
+        "FIELD1": "Season 9, Episode 6",
+        "FIELD2": "The Boat"
+    },
+    {
+        "FIELD1": "Season 9, Episode 7",
+        "FIELD2": "The Whale"
+    },
+    {
+        "FIELD1": "Season 9, Episode 8",
+        "FIELD2": "The Target"
+    },
+    {
+        "FIELD1": "Season 9, Episode 9",
+        "FIELD2": "Dwight Christmas"
+    },
+    {
+        "FIELD1": "Season 9, Episode 10",
+        "FIELD2": "Lice"
+    },
+    {
+        "FIELD1": "Season 9, Episode 11",
+        "FIELD2": "Suit Warehouse"
+    },
+    {
+        "FIELD1": "Season 9, Episode 12",
+        "FIELD2": "Customer Loyalty"
+    },
+    {
+        "FIELD1": "Season 9, Episode 13",
+        "FIELD2": "Junior Salesman"
+    },
+    {
+        "FIELD1": "Season 9, Episode 14",
+        "FIELD2": "Vandalism"
+    },
+    {
+        "FIELD1": "Season 9, Episode 15",
+        "FIELD2": "Couple's Discount"
+    },
+    {
+        "FIELD1": "Season 9, Episode 16",
+        "FIELD2": "Moving On"
+    },
+    {
+        "FIELD1": "Season 9, Episode 17",
+        "FIELD2": "The Farm"
+    },
+    {
+        "FIELD1": "Season 9, Episode 18",
+        "FIELD2": "Promos"
+    },
+    {
+        "FIELD1": "Season 9, Episode 19",
+        "FIELD2": "Stairmageddon"
+    },
+    {
+        "FIELD1": "Season 9, Episode 20",
+        "FIELD2": "Paper Airplane"
+    },
+    {
+        "FIELD1": "Season 9, Episode 21",
+        "FIELD2": "Livin' the Dream"
+    },
+    {
+        "FIELD1": "Season 9, Episode 22",
+        "FIELD2": "A.A.R.M"
+    },
+    {
+        "FIELD1": "Season 9, Episode 23",
+        "FIELD2": "Finale"
+    }
+]
